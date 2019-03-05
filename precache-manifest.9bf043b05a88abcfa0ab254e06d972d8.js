5c143fa1f448d7b8054c541a2e00d0a179d4d631
Carlo James Lim <carlo@sprout.ph>
2019-03-05 17:18:31 +0800

    Updates

---
+++ precache-manifest.9bf043b05a88abcfa0ab254e06d972d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/carlolim/admin1password1/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7a97462d9657e65045da",
+    "url": "/carlolim/admin1password1/static/js/runtime~main.7a97462d.js"
+  },
+  {
+    "revision": "250e90287bd75c93dc34",
+    "url": "/carlolim/admin1password1/static/js/main.250e9028.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/carlolim/admin1password1/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "250e90287bd75c93dc34",
+    "url": "/carlolim/admin1password1/static/css/main.341b03c3.chunk.css"
+  },
+  {
+    "revision": "62172e00380f1e04716efcd7495be981",
+    "url": "/carlolim/admin1password1/index.html"
+  }
+];
